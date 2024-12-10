901c4e8b1256f858a16176a078f500dac6264d1c
Ladislav Jaromír Pleštil <48877289+ladislavpl@users.noreply.github.com>
2024-12-10 10:15:43 +0100

    Updated to version 1.2
    Přidán factoriál a pythagorova věta.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main(func, operationName):
             i = i + 1
         print("Výsledek: ", c, "\n")
 
-version = "1.1"
+version = "1.2"
 
 print("Kalkulačka")
 print("Verze: ", version, "\n")
@@ -37,6 +37,8 @@ while True:
             print("5 - Mocnina")
             print("6 - Odmocnina")
             print("7 - Výpis násobilky")
+            print("8 - Faktoriál")
+            print("9 - Pythagorova věta")
             print("exit - Ukončit aplikaci")
             print("help - Výpis funkcí")
             print("\n")
@@ -69,5 +71,36 @@ while True:
                 arrayNasobilka.append(i * nasobilka)
                 i = i + 1
             print("Výpis násobilky: ", arrayNasobilka, "\n")
+        case "8":
+            a = int(input("Zadej číslo: "))
+            c = a
+            i = a - 1
+            while i >= 1:
+                c = c * i
+                i = i - 1
+            print("Výsledek: ", c, "\n")
+        case "9":
+            print("1. c² = a² + b²")
+            print("2. a² = c² - b²")
+            print("3. b² = c² - a²")
+            akce = int(input("Vyber akci: "))
+            match akce:
+                case 1:
+                    a = float(input("Zadej a: "))
+                    b = float(input("Zadej b: "))
+                    c = (a ** 2 + b ** 2) ** (1 / 2)
+                    print("c = ", c, "\n")
+                case 2:
+                    c = float(input("Zadej c: "))
+                    b = float(input("Zadej b? "))
+                    a = (c ** 2 - b ** 2) ** (1 / 2)
+                    print("a = ", a, "\n")
+                case 3:
+                    c = float(input("Zadej c: "))
+                    a = float(input("Zadej a: "))
+                    b = (c ** 2 - a ** 2) ** (1 / 2)
+                    print("b = ", b, "\n")
+                case _:
+                    print("Neplatná operace\n")
         case _:
             print("Neplatná operace\n")
